24de969a92ae577300cc9f66b95b1cf48d99ec2c
Jamin Yang <jaminyang2000@gmail.com>
2024-06-20 12:04:58 -0400

    Readme, CSS and JS changes

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+- Git clone this repository
+- Enter measurements into number inputs and click submit
+
+![demo](demo-1.png)
+![demo](demo-2.png)
--- main.js
+++ main.js
@@ -1,54 +1,67 @@
-var checkbox = document.getElementById('switchMeasure');
-const measure1 = document.querySelector('.defaultMeasure');
-const measure2 = document.querySelector('.altMeasure');
-const submit = document.getElementById('submitBox');
-const BMI_info = document.querySelector('.BMI-container')
-const BMI_num = document.getElementById('BMI-number');
+var checkbox = document.getElementById("switchMeasure");
+const measure1 = document.querySelector(".defaultMeasure");
+const measure2 = document.querySelector(".altMeasure");
+const submit = document.getElementById("submitBox");
+const BMI_info = document.querySelector(".BMI-container");
+const BMI_num = document.getElementById("BMI-number");
 
-checkbox.addEventListener('change', ()=>{
-    validateSwitchMeasure();
+// Prevent typing letters into number inputs
+document.querySelectorAll("input").forEach((text) => {
+  text.addEventListener("keypress", function (e) {
+    var allowedChars = "0123456789";
+    function contains(stringValue, charValue) {
+      return stringValue.indexOf(charValue) > -1;
+    }
+    var invalidKey =
+      (e.key.length === 1 && !contains(allowedChars, e.key)) ||
+      (e.key === "." && contains(e.target.value, "."));
+    invalidKey && e.preventDefault();
+  });
 });
 
-submit.addEventListener('click', ()=>{
-    outputBMI();
+checkbox.addEventListener("change", () => {
+  validateSwitchMeasure();
 });
 
-function validateSwitchMeasure(){
-    if (checkbox.checked){
-        measure1.style.display ='none';
-        measure2.style.display = 'block';
-    }else{
-        measure2.style.display ='none';
-        measure1.style.display = 'block';
-    }
-};
+submit.addEventListener("click", () => {
+  outputBMI();
+});
 
-function outputBMI(){
-    if (checkbox.checked){
-        const weight = measure2.children[1].children[0].value;
-        const height = measure2.children[0].children[0].value;
-        if (!weight || !height){
-            alert("Please enter your measurements");
-            return;
-        }
-        let result = parseInt(weight) / (parseInt(height) /100) **2;
-        BMI_num.textContent = result.toFixed(1);
-        BMI_info.style.display='flex';
-        //weight (kg) / [height (m)]^2
-    }else{
-        // console.log(measure1.children[1].children);
-        const weight = measure1.children[1].children[0].value;
-        const height = measure1.children[0].children;
-        const feet = height[0].value;
-        const inches = height[1].value;
-        if (!weight || !feet || !inches){
-            alert("Please enter your measurements");
-            return;
-        }
-        let result = (parseInt(weight) / (parseInt(feet) * 12 + parseInt(inches)) **2) *703;
-        BMI_num.textContent = result.toFixed(1);
-        BMI_info.style.display='flex';
+function validateSwitchMeasure() {
+  if (checkbox.checked) {
+    measure1.style.display = "none";
+    measure2.style.display = "block";
+  } else {
+    measure2.style.display = "none";
+    measure1.style.display = "block";
+  }
+}
 
-        //weight (lb) / [height (in)]^2 x 703
+function outputBMI() {
+  if (checkbox.checked) {
+    const weight = document.getElementById('altWeight').value;
+    const height = document.getElementById('altHeight').value;
+    console.log(weight, height);
+    if (!weight || !height) {
+      alert("Please enter your measurements");
+      return;
     }
+    let result = parseInt(weight) / (parseInt(height) / 100) ** 2;
+    BMI_num.textContent = result.toFixed(1);
+    BMI_info.style.display = "flex";
+    //weight (kg) / [height (m)]^2
+  } else {
+    const feet = document.getElementById('defHeight-1').value;
+    const inches = document.getElementById('defHeight-2').value;
+    const weight = document.getElementById('defWeight').value;
+    if (!weight || !feet || !inches) {
+      alert("Please enter your measurements");
+      return;
+    }
+    let result =
+      (parseInt(weight) / (parseInt(feet) * 12 + parseInt(inches)) ** 2) * 703;
+    BMI_num.textContent = result.toFixed(1);
+    BMI_info.style.display = "flex";
+    //weight (lb) / [height (in)]^2 x 703
+  }
 }
